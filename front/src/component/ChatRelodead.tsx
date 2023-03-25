8ee6e91ae471a80d8ea1ba8c5b70db0f522e01d4
Inonnalla <isbled.arielle@gmail.com>
2023-03-25 18:08:19 +0100

    le TABLEAU

--- front/src/component/ChatRelodead.tsx
+++ front/src/component/ChatRelodead.tsx
@@ -2,19 +2,33 @@ import { useState } from 'react';
 
 const ChatRelodead = () => {
   const [value, setValue] = useState(' ici');
+  const [temara, setTemara] = useState<string[]>([]);
+  console.log(temara);
   return (
     <>
+      <p>{temara}</p>
+
       <input
         value={value}
         onChange={(ev) => {
-          //   setValue(ev.target.value);
-          setValue((previouValue: string) => {
-            console.log(previouValue);
-          });
+          setValue(ev.target.value);
+          //   setValue((previouValue: string) => {
+          //     console.log(previouValue);
+          //     return ev.target.value;
+          //   });
         }}
       ></input>
 
-      <button>Envoye</button>
+      <button
+        onClick={() =>
+          setTemara((previouTemara) => {
+            previouTemara.push(value);
+            return [...previouTemara];
+          })
+        }
+      >
+        Envoye
+      </button>
     </>
   );
 };
