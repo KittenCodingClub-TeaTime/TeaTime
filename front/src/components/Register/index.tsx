56d2ad7c8e0fcfa3f7c5585d3c0a9431850f4310
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-05-23 15:46:28 +0200

    :construction: register front done ?

--- front/src/components/Register/index.tsx
+++ front/src/components/Register/index.tsx
@@ -1,105 +1,84 @@
 import { FC } from 'react';
 import styles from '../../styles/Username.module.css';
-import Avatar from '../Avatar';
-import { Formik, Field, FastField, Form } from 'formik';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast, { Toaster } from 'react-hot-toast';
+import { z } from 'zod';
+import { Link } from 'react-router-dom';
 
-import * as Yup from 'yup';
+const schema = z
+  .object({
+    username: z.string().min(4, 'Username doit contenir au moins 4 characters'),
+    email: z.string().email({ message: 'Adresse mail invalide' }),
+    password: z.string().min(6, { message: 'Password must be atleast 6 characters' }),
+    confirmPassword: z.string().min(1, { message: 'Passwords must match' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: "Password don't match",
+  });
+
+interface MyInputTypes {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register: FC = ({}) => {
-  /* const formik = useFormik({
-    initialValues: {
-      username: '',
-    },
-
-    validationSchema: Yup.object({
-      username: Yup.string()
-        .min(4, 'Must be more than 3 character')
-        .max(6, 'Name must be 20 characters or less.')
-        .required('Name is required'),
-      email: Yup.string().email('Invalid email address').required('Email is required'),
-      terms: Yup.array().required('Terms of service must be checked'),
-    }),
-
-    onSubmit: (values: any) => {
-      console.log('form submitted');
-      console.log(values);
-    },
-  }); */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<MyInputTypes>({
+    resolver: zodResolver(schema),
+  });
+  const handleSave = (data: any) => {
+    toast.success('Register Successfully...!');
+    console.log(data);
+    //TODO: SEND THE VALUES TO THE BACK
+  };
 
   return (
-    <div className="container mx-auto border">
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+
       <div className="flex justify-center items-center h-screen">
-        <div className={styles.glass}>
+        <div className={styles.glass} style={{ width: '45%', paddingTop: '3em' }}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello Again!</h4>
-            <span className="py-4 text-xl w-2/3 text-center text-gray-500"> Explore More by connecting with us.</span>
+            <h4 className="text-5xl font-bold">Register</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">Happy to see you!</span>
           </div>
-          <Formik
-            initialValues={{
-              firstName: '',
-              lastName: '',
-              email: '',
-            }}
-            validationSchema={Yup.object().shape({
-              username: Yup.string().min(2, 'npapap 2').max(6, 'nanana 6').required(),
-            })}
-            onSubmit={(values) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-              }, 500);
-            }}
-            render={(formikProps) => (
-              <Form className="fpy-1 text-center">
-                <div className="profile flex justify-center py-4">
-                  <Avatar />
-                </div>
-                <div className="textbox flex flex-col items-center gap-6">
-                  <label htmlFor="username">Username</label>
-                  <FastField name="username" placeholder="Weezy">
-                    {({ field, form, meta }: any) => (
-                      <div>
-                        <input placeholder="username" {...field} className={styles.textbox} />
-                        {/**
-                         * This updates normally because it's from the same slice of Formik state,
-                         * i.e. path to the object matches the name of this <FastField />
-                         */}
-                        {meta.touched ? meta.error : null}
 
-                        {/** This won't ever update since it's coming from
-                  from another <Field>/<FastField>'s (i.e. firstName's) slice   */}
-                        {form.touched.username && form.errors.username ? form.errors.username : null}
+          <form className="py-1" onSubmit={handleSubmit(handleSave)}>
+            <div className="textbox flex flex-col items-center gap-4">
+              <input {...register('email')} className={styles.textbox} type="text" placeholder="Email*" />
+              {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+              <input {...register('username')} className={styles.textbox} type="text" placeholder="Username*" />
+              {errors.username && <p className={styles.error}>{errors.username.message}</p>}
+              <input {...register('password')} className={styles.textbox} type="password" placeholder="Password*" />
+              {errors.password && <p className={styles.error}>{errors.password.message}</p>}
+              <input
+                {...register('confirmPassword')}
+                className={styles.textbox}
+                type="password"
+                placeholder="confirmPassword*"
+              />
+              {errors.confirmPassword && <p className={styles.error}>{errors.confirmPassword.message}</p>}
+              <button className={styles.btn} type="submit">
+                Register
+              </button>
+            </div>
 
-                        {/* Imperative methods still work as expected */}
-                        <button className={styles.btn} type="submit">
-                          Let's Go
-                        </button>
-                      </div>
-                    )}
-                  </FastField>
-                  {/* <input
-                    className={styles.textbox}
-                    id="username"
-                    type="text"
-                    placeholder="username"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.username}
-                  />
-                  <button className={styles.btn} type="submit">
-                    Let's Go
-                  </button> */}
-                </div>
-                <div className="text-center py-4">
-                  <span className="text-gray-500">
-                    Not a Member{' '}
-                    <button type="submit" className="text-red-600 ml-1">
-                      Register now
-                    </button>
-                  </span>
-                </div>
-              </Form>
-            )}
-          />
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Already registered ?{' '}
+                <Link className="text-red-500" to="/login">
+                  Login Now
+                </Link>
+              </span>
+            </div>
+          </form>
         </div>
       </div>
     </div>
