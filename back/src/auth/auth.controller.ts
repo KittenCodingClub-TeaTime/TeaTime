52c1107aaec42114a6e78cc4c1655efa01c11c12
LEO GOUBARD <goubardleo@gmail.com>
2023-06-09 16:51:59 +0200

    :white_check_mark: ready to pr

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,15 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from '../../shared/dtos/users/create-users.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Post('signup')
-  signup(@Body() dto: CreateUserDto) {
-    return this.authService.signup(dto);
-  }
 
   @Post('signin')
   signin() {
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,44 +1,13 @@
 import { PrismaService } from '@/prisma.service';
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from '../../shared/dtos/users/create-users.dto';
-import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  async signup(dto: CreateUserDto) {
-    const { email, password, name } = dto;
-    const foundUser = await this.prisma.user.findUnique({ where: { email } });
-
-    if (foundUser) {
-      throw new BadRequestException('Email already exists');
-    }
-    try {
-      const hashedPassword = await this.hashPassword(password);
-      await this.prisma.user.create({
-        data: {
-          email,
-          name,
-          hashedPassword,
-        },
-      });
-      return { message: 'User register sucessfully' };
-    } catch (error) {
-      throw new BadRequestException('Email already exists');
-    }
-  }
   async signin() {
     return '';
   }
   async signout() {
     return '';
-  }
-
-  async hashPassword(password: string) {
-    const saltOrRounds = 10;
-
-    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
-
-    return hashedPassword;
   }
 }
